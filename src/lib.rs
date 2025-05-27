2ea7e7887ceb34ad4d91e92eb81d263fa0331eff
jtb <bloo1987@proton.me>
2025-05-26 17:10:18 -0700

    naming

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ pub mod python_ffi;
 
 // Define the Python module that exposes all Pyo3 API to python users.
 #[pymodule]
-fn my_extension(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+#[pyo3(name = "jpe_python")]
+fn py_module(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     config::register_pyo3(py, m)?;
     base::register_pyo3(py, m)?;
     python_ffi::register_pyo3(py, m)?;

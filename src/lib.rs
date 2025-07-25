1933fc459e075424fbe321677805f760cae9666b
jtb <bloo1987@proton.me>
2025-07-25 11:16:06 -0700

    changed python package name

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod python_ffi;
 
 // Define the Python module that exposes Pyo3 API to python users.
 #[pymodule]
-#[pyo3(name = "jpe_python")]
+#[pyo3(name = "jpe_python_ffi")]
 fn py_module(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     config::register_pyo3(py, m)?;
     base::register_pyo3(py, m)?;

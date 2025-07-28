7a6fd8eb1f8e2fbe94b19229fcfc21a9219ea7bb
jtb <bloo1987@proton.me>
2025-07-28 12:15:19 -0700

    API testing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use jpe::BaseContextBuilder;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut ctx = BaseContextBuilder::new()
+        .with_serial("/dev/my_usb")
+        .build()?;
+    Ok(())
+}

35db7e2a3056117cb11e79b518905aedef74e1a0
jtb <bloo1987@proton.me>
2025-07-31 15:35:44 -0700

    main examples

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use jpe::BaseContextBuilder;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut ctx = BaseContextBuilder::new()
-        .with_serial("/dev/my_usb")
+        .with_serial_async("/dev/my_usb")
+        .build()?;
+    let mut ctx = BaseContextBuilder::new()
+        .with_network_async("192.168.10.1")?
         .build()?;
     Ok(())
 }

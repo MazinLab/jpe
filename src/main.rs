ed112686ebaabb691b3a23fc00904dea06c4e154
jtb <bloo1987@proton.me>
2025-08-04 12:40:47 -0700

    added jank tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use jpe::BaseContextBuilder;
 #[cfg(feature = "sync")]
-fn main() {}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Building context");
+    //let mut ctx = BaseContextBuilder::new()
+    //    .with_serial("/dev/cu.usbserial-D30IYJT2")
+    //    .build()?;
+    let mut ctx = BaseContextBuilder::new()
+        .with_network("192.168.10.5")?
+        .build()?;
+    println!("Context built");
+    println!("{:?}", ctx.get_fw_version()?);
+    println!("{:?}", ctx.get_module_list()?);
+    println!("{:?}", ctx.get_supported_stages()?);
+    println!("{:?}", ctx.get_ip_config()?);
+    println!("{:?}", ctx.get_mod_fw_version(jpe::Slot::One)?);
+    println!("{:?}", ctx.get_fail_safe_state(jpe::Slot::One)?);
+    //println!("{:?}", ctx.set_ip_config(jpe::IpAddrMode::Static, "192.168.10.5", "255.255.255.0", "192.168.10.1").await?);
+
+    Ok(())
+}
 
 #[cfg(feature = "async")]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Building context");
-    //let mut ctx = BaseContextBuilder::new()
-    //    .with_serial_async("/dev/cu.usbserial-D30IYJT2")
-    //    .build()
-    //    .await?;
+    println!("Building async context");
     let mut ctx = BaseContextBuilder::new()
-        .with_network_async("192.168.10.5")?
+        .with_serial_async("/dev/cu.usbserial-D30IYJT2")
         .build()
         .await?;
-    println!("Context built");
+    //let mut ctx = BaseContextBuilder::new()
+    //    .with_network_async("192.168.10.5")?
+    //    .build()
+    //    .await?;
+    println!("Async context built");
     println!("{:?}", ctx.get_fw_version().await?);
     println!("{:?}", ctx.get_module_list().await?);
     println!("{:?}", ctx.get_supported_stages().await?);
